39d3dd4467a30c80f397472f3a328eed72473424
Eric Warrington <ericwarrington@users.noreply.github.com>
2023-04-09 23:10:45 -0400

    Added MaterialModule for easier access to material components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,16 @@
+import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
+
 import { AppComponent } from "./app.component";
 import { BackendlessMockService } from "./backendless-mock.service";
 import { EmployeeComponent } from "./employee/employee.component";
 import { EmployeeListComponent } from "./employee-list/employee-list.component";
 import { EmployeeService } from "./employee.service";
-import { MatCardModule } from "@angular/material/card";
-import { MatGridListModule } from "@angular/material/grid-list";
-import { MatInputModule } from "@angular/material/input";
-import { MatMenuModule } from "@angular/material/menu";
-import { MatIconModule } from "@angular/material/icon";
-import { MatButtonModule } from "@angular/material/button";
+import { MaterialModule } from "./material.module.ts";
 
 @NgModule(
 {
@@ -38,12 +34,7 @@ import { MatButtonModule } from "@angular/material/button";
 			post204: false,
 			put204: false
 		}),
-		MatCardModule,
-		MatGridListModule,
-		MatInputModule,
-		MatMenuModule,
-		MatIconModule,
-		MatButtonModule
+		MaterialModule,
 	],
 	providers: [EmployeeService],
 	bootstrap: [AppComponent]
